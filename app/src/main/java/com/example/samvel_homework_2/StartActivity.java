e46f6defc76eba73f0fae972e82d6cb1a0ea83da
SamvelPapyan15a <samuel.papyan15a@gmail.com>
2021-10-18 14:21:50 +0400

    first commit

---
+++ app/src/main/java/com/example/samvel_homework_2/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.samvel_homework_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class StartActivity extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
